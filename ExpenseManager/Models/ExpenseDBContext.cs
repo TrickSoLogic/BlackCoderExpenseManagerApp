0f167d2d8725045b3e16493ed62b21472f888d90
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-19 23:46:24 +0530

    Add project files.

---
+++ ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackCoderExpenseManagerApp.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
